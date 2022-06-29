98d336add82c4922ed51ff96c04cc8c46697466b
Mathias Leandro Fernandez Bo <mathias.fernandez.bo@fing.edu.uy>
2022-06-29 07:24:23 -0300

    fix perfil

--- view/perfilCliente.php
+++ view/perfilCliente.php
@@ -33,8 +33,7 @@
           
             <h2> Historial de compras </h2>
 
-            <?php if (isset($compras)) { ?>     
-                                    
+            <?php if (isset($compras)) { ?>                                        
                     <table border="1" class="table table-hover">
                     <thead>       
                         <th><strong>Compra</strong></th>                      
@@ -44,12 +43,12 @@
                         <th><strong>Producto</strong></th>                            
                     </tr></thead>
                     <tbody id="myTable">
-                    <?php for ($i=0; $i < count($compras); $i++) { ?>                      
+                    <?php $j=0; while ($j < count($compras)) { ?>                      
                         <tr style="display: table-row;">  
-                            <td> <?php echo '#' . $compras[$i]['idCompra'] ?> </td>
-                            <td> <?php echo $compras[$i]['metodo'] ?> </td>
-                            <td> <?php echo $compras[$i]['total'] ?> </td>
-                            <td> <?php echo $compras[$i]['fecha'] ?> </td>
+                            <td> <?php echo '#' . $compras[$j]['idCompra'] ?> </td>
+                            <td> <?php echo $compras[$j]['metodo'] ?> </td>
+                            <td> <?php echo $compras[$j]['total'] ?> </td>
+                            <td> <?php echo $compras[$j]['fecha'] ?> </td>
                             <td> 
                             <table border="1" class="table table-hover">
                                 <thead>   
@@ -57,25 +56,26 @@
                                  <th><strong>*</strong></th>
                                  </tr></thead>
                                  <tbody id="myTable">
-                                 <?php $productos = $cliente -> listarProductosUsuario($compras[$i]['idCompra']);   
-                                    if (isset($productos)) {  
-                                        for ($i=0; $i < count($productos); $i++) { ?>                           
-                                            <tr style="display: table-row;">  
-                                                <td> <?php echo $productos[$i]['producto'] ?> </td> 
-                                                <?php if (!$cliente->checkComentario($productos[$i]['producto'], $_SESSION['correo'])) { ?>
+                                 <?php $productos = $cliente -> listarProductosUsuario($compras[$j]['idCompra']); 
+                                       $j++;  
+                                       if (isset($productos)) {  
+                                            $i=0; 
+                                            do { ?>                           
+                                                <tr style="display: table-row;">  
+                                                    <td> <?php echo $productos[$i]['producto'] ?> </td> 
+                                                    <?php if (!$cliente->checkComentario($productos[$i]['producto'], $_SESSION['correo'])) { ?>
                                                         <td> <a href="/TB-ROOT/view/comentarProducto.php?id=<?php echo $productos[$i]['producto']?>"> <input class="btn btn-warning" type="button" value="Valorar producto"/> </a> </td>                                         
                                                     <?php } else { ?> <td> <button disabled class="btn btn-warning" type="button"> Valorar producto </button> </td> <?php } ?> <td>                                     
-                                                </tr>  
-                                        <?php }
+                                                    </tr>  
+                                        <?php $i++; } while ($i < count($productos));
                                         } ?>
-                                    </tbody></table>                                        
-                                </tr>                                            
-                            <?php } ?>
-                            </tbody></table>                          
-                        </div> 
-                    <?php } ?>     
-            
-           
+                                </tbody></table>                                        
+                        </tr>                                            
+                    <?php                         
+                         } ?>
+                    </tbody></table>                          
+                </div> 
+            <?php } ?>        
         
   
 
