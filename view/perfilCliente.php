051a0ac3ae795276b4d1b2407a4ed3e73229eab7
Mathias Leandro Fernandez Bo <mathias.fernandez.bo@fing.edu.uy>
2022-06-29 08:01:15 -0300

    fix

--- view/perfilCliente.php
+++ view/perfilCliente.php
@@ -67,7 +67,7 @@
                                                     <td> <a href="/TB-ROOT/view/comentarProducto.php?id=<?php echo $productos[$i]['producto']?>"> <input class="btn btn-warning" type="button" value="Valorar producto"/> </a> </td>                                         
                                                 <?php } else { ?> <td> <button disabled class="btn btn-warning" type="button"> Valorar producto </button> </td> <?php } ?> <td>                                     
                                                 </tr>  
-                                    <?php $i++; $j++; } while ($i < count($productos));
+                                    <?php $i++;$j+$i;} while ($i < count($productos));
                                     } ?>
                                     </tbody></table>                                        
                         </tr>                                            
