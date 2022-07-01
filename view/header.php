b460cdca3dc6bd481380cbfadc1cbefe30fe3cca
Mathias Leandro Fernandez Bo <mathias.fernandez.bo@fing.edu.uy>
2022-06-30 23:29:05 -0300

    fix table logo

--- view/header.php
+++ view/header.php
@@ -293,7 +293,7 @@ button:active {
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-  <a class="navbar-brand ms-3" href="/TB-ROOT/index.php"><img src="/TB-ROOT/view/resources/tecno-bay.jpg" width="80" class="img-thumbnail"/></a>
+  <a class="navbar-brand ms-3" href="/TB-ROOT/index.php"><img src="/TB-ROOT/view/resources/tecno-bay.jpg" class="img-thumbnail"/></a>
   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
